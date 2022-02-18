2e1819de5562567aaee12154d304def1abfd5875
HenriPC <henriquechimetta@outlook.com>
2022-02-18 16:29:12 -0300

    added mobile solution for article preview component challenge

--- article-preview-component/index.js
+++ article-preview-component/index.js
@@ -1,7 +1,10 @@
 const iconShare = document.querySelector('.icon-share');
-const socialContainer = document.querySelector('.socials-container');
 const iconContainer = document.querySelector(".icon-container");
+const socialContainer = document.querySelector('.socials-container');
 const body = document.querySelector('body');
+const iconContainerMobile = document.querySelector(".icon-container-mobile");
+const iconShareMobile = document.querySelector('.icon-share-mobile');
+const socialContainerMobile = document.querySelector('.socials-container-mobile');
 
 iconContainer.addEventListener('mouseover', e => {
     iconShare.classList.add('icon-share-white');
@@ -12,11 +15,28 @@ iconContainer.addEventListener('mouseout', e => {
 });
 
 iconContainer.addEventListener('click', e => {
-    socialContainer.classList.add('show');
+    if (window.innerWidth <= 800) {
+        socialContainerMobile.classList.remove('hide-animation');
+        socialContainerMobile.classList.add('show-animation');
+    } else {
+        socialContainer.classList.add('show');
+    }
     e.stopPropagation();
 });
 
 body.addEventListener('click', e => {
-    socialContainer.classList.remove('show');
+    if (window.innerWidth <= 800) {
+        socialContainerMobile.classList.remove('show-animation');
+        socialContainerMobile.classList.add('hide-animation');
+    } else {
+        socialContainer.classList.remove('show');
+    }
 });
 
+iconContainerMobile.addEventListener('mouseover', e => {
+    iconShareMobile.classList.add('icon-share-dark');
+});
+
+iconContainerMobile.addEventListener('mouseout', e => {
+    iconShareMobile.classList.remove('icon-share-dark');
+});
