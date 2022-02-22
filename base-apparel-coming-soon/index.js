c200d96e2cdf5e010f3e54c1cc4714b6c1fb4be8
HenriPC <henriquechimetta@outlook.com>
2022-02-22 18:24:55 -0300

    fixed issues desktop version for base apparel challenge

--- base-apparel-coming-soon/index.js
+++ base-apparel-coming-soon/index.js
@@ -1,9 +1,22 @@
-document.querySelector(".email").addEventListener("invalid", e => {
+const invalidMessage = document.querySelector(".invalid-phrase");
+const invalidIcon = document.querySelector('.invalid-img');
+const email = document.querySelector(".email");
+const nav = document.querySelector('.nav');
+const headingContainer = document.querySelector('.heading-container');
+const imgContainer = document.querySelector('.img-container');
+
+const containers = [nav, headingContainer, imgContainer];
+
+email.addEventListener("invalid", e => {
     e.preventDefault();
+    invalidMessage.classList.add('show');
+    invalidIcon.classList.add('show');
+    e.stopPropagation();
 });
 
-document.querySelector(".form").addEventListener("submit", function (event) {
-    if (!this.checkValidity()) {
-        event.preventDefault();
-    }
+containers.forEach(elem => {
+    elem.addEventListener('click', e => {
+        invalidMessage.classList.remove('show');
+        invalidIcon.classList.remove('show');
+    });
 });
